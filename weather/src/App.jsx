3afc33ad689521f2efff8228d135d96649a1fe11
ParkerWW <Parkerjw0@gmail.com>
2025-01-25 17:52:26 -0800

    Textbox and button functionality

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,5 +1,7 @@
 import Logo from '/Logo.png'
 import './App.css'
+import React from 'react'
+import Button from 'react'
  
 //jquery import
 var script = document.createElement('script');
@@ -7,9 +9,11 @@ script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jqu
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function App() {
+  let place = "Seattle Washington"
   //get data from api
   function getData() {
-  $.get("https://api.weatherapi.com/v1/current.json?key=703225d7d1284e52b05235515251701&q=83835&aqi=no")
+  $.get("https://api.weatherapi.com/v1/current.json?key=703225d7d1284e52b05235515251701&q=" + place + "&aqi=no")
+  
   //$.get("https://jsonplaceholder.typicode.com/posts/1") 
   .done(function(response) {
     //console.log(response)
@@ -20,7 +24,7 @@ function App() {
   });
 }
 
-//put api into html by ID
+//put api response on webpage
 function processData(response){
   //get location, current temp, and weather icon
   let loc = response.location.name + ", " + response.location.region
@@ -34,29 +38,37 @@ function processData(response){
 }
 
 function handleChange() {
-  let newLoc = document.getElementById("locText").value
-  //console.log(newLoc)
+  //clicking submit button to update location
+  place = document.getElementById("locText").value
+  //console.log(place)
   getData()
 }
 
+function onEnter() {
+  //same as clicking submit button, but hitting enter in the textbox
+  if(event.key === 'Enter'){
+    handleChange()
+  }
+}
+
+//fetch data on page load
 getData()
 
   return (
     <>
       <header className="header">
         <img src={Logo} className="logo" alt="logo" />
-        <p className="legal">
-          Could also be inside
-        </p>
+        <p className="legal"> Could also be inside </p>
+
         <div className="textboxC">
-          <input className="texbox" type="text" id="locText" name="LocationText" onChange={handleChange} placeholder="Enter Location" />
+          <input className="textbox" type="text" id="locText" name="LocationText" placeholder="Enter Location" onKeyDown={onEnter} />
+          <button className="textbutton" type="submit" onClick={handleChange}> Submit </button>
         </div>
       </header>
-      <div className="container">
-        <h1 id="temp" />
-        <img id="icon" width="80" height="auto" />
-      </div>
-      <p id="location" />
+      
+      <img id="icon" width="80" height="auto" />
+      <h1 className='info' id="temp" />
+      <p className='info' id="location" />
     </>
   )
 }
